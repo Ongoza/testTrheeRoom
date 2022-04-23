0b433d96cf8612ca021649c12e5c4b2b1af16b74
oleg@ongoza.com <oleg@ongoza.com>
2022-04-23 17:05:41 +0300

    Init commit

---
+++ server.py
@@ -0,0 +1,40 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)        
+        if self.path.find(".js")!=-1:            
+            self.send_header("Content-type", "text/javascript")
+        else:
+            self.send_header("Content-type", "text/html")
+
+        self.end_headers()
+
+        #self.wfile.write("<html><head><title>Title goes here.</title></head></html>".encode("utf-8"))
+
+        f = open('./'+self.path, "rb")
+        self.wfile.write(f.read())
+        
+        #self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        #print(self.path)
+        #self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        #self.wfile.write(bytes("<body>", "utf-8"))
+        #self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        #self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
